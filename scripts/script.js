413a914f50f975b21fbd9324b228a7c1ecbad586
CardinalCandle <youssef-kj@outlook.fr>
2021-07-13 16:27:36 +0100

    Updated script.js and style.css

--- scripts/script.js
+++ scripts/script.js
@@ -36,16 +36,40 @@ function resetBoard(){
 }
 function resetGrid() {
     numo = prompt("Please insert the size of your desired grid: (Number between 1-100)");
+    if (parseInt(numo) > 100) {
+        alert("Number exeeds the limit, Please try again")
+         resetGrid;
+    }
+    else if (parseInt(numo) < 0) {
+        alert("Number is a negative, Please try again")
+         resetGrid;
+    }
+    else {
+    removeAllChildNodes(document.getElementById("container"))
     document.getElementById("container").style.gridTemplateRows = ""
     document.getElementById("container").style.gridTemplateColumns = ""
-    num = parseInt(numo) + 1
-    for(y=1; y < num; y++) {
-        document.getElementById("container").style.gridTemplateRows += `${Math.floor(100/num)}%`
-        console.log('loop row number '+y)
-    }
+num = parseInt(numo) + 1
     numsq = numo*numo ; numsq += 1;
+    for(y=1; y < num; y++) {
+        document.getElementById("container").style.gridTemplateRows += ` ${Math.floor(500/num)}px `
+       document.getElementById("container").style.gridTemplateColumns += ` ${Math.floor(500/num)}px `
+       console.log('loop row number '+y)
+    }
     for(i=1; i<numsq; i++) {
+        console.log("addin element "+ i)
         addElement(i)
     }
+    childs = document.getElementById("container").children;
+    //childs.style.backgroundColor = ""
+    for (i = 0; i < childs.length; i++) {
+      childs[i].style.value = `width :${40/childs.length}rem`; //height :${40/childs.length}rem `
+     }
+    }
+    }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
+resetGrid()
